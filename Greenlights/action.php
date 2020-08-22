3d8b32787a7bd4cc5965b4b5406a9b0c9b4a13b4
Andrey Staniukynas <dev.staniukynas@gmail.com>
2020-08-22 15:13:59 +0300

    old but with no sample files

--- Greenlights/action.php
+++ Greenlights/action.php
@@ -2,30 +2,48 @@
 include_once("inc/db_connect.php");
 if ($_POST['action'] == 'edit' && $_POST['id']) {	
 	$updateField='';
-	if(isset($_POST['name'])) {
-		$updateField.= "name='".$_POST['name']."'";
-	} else if(isset($_POST['gender'])) {
-		$updateField.= "gender='".$_POST['gender']."'";
-	} else if(isset($_POST['age'])) {
-		$updateField.= "age='".$_POST['age']."'";
+	if(isset($_POST['week'])) {
+		$updateField.= "week='".$_POST['week']."'";
+	} else if(isset($_POST['session'])) {
+		$updateField.= "session='".$_POST['session']."'";
+    } else if(isset($_POST['task'])) {
+		$updateField.= "task='".$_POST['task']."'";
+    } else if(isset($_POST['group_number'])) {
+		$updateField.= "group_number='".$_POST['group_number']."'";
+    } else if(isset($_POST['rating'])) {
+		$updateField.= "rating='".$_POST['rating']."'";
+	} else if(isset($_POST['rating'])) {
+		$updateField.= "rating='".$_POST['rating']."'";
+	} else if(isset($_POST['task_expected'])) {
+		$updateField.= "task_expected='".$_POST['task_expected']."'";
+	} else if(isset($_POST['task_actual'])) {
+		$updateField.= "task_actual='".$_POST['task_actual']."'";
+	} else if(isset($_POST['comment'])) {
+		$updateField.= "comment='".$_POST['comment']."'";
+	} else if(isset($_POST['action'])) {
+		$updateField.= "action='".$_POST['action']."'";
+	} else if(isset($_POST['meeting_date'])) {
+		$updateField.= "meeting_date='".$_POST['meeting_date']."'";
+	} else if(isset($_POST['meeting_duration'])) {
+		$updateField.= "meeting_duration='".$_POST['meeting_duration']."'";
 	}
 	if($updateField && $_POST['id']) {
-        echo "HEY";
-		$sqlQuery = "UPDATE developers SET $updateField WHERE id='" . $_POST['id'] . "'";	
+		$sqlQuery = "UPDATE s18182839 SET $updateField WHERE id='" . $_POST['id'] . "'";	
 		mysqli_query($conn, $sqlQuery) or die("database error:". mysqli_error($conn));	
 		$data = array(
-			"message"	=> "Record Updated",	
+			"messrating"	=> "Record Updated",	
 			"status" => 1
 		);
 		echo json_encode($data);		
 	}
 }
 if ($_POST['action'] == 'delete' && $_POST['id']) {
-	$sqlQuery = "DELETE FROM developers WHERE id='" . $_POST['id'] . "'";	
+	$sqlQuery = "DELETE FROM s18182839 WHERE id='" . $_POST['id'] . "'";	
 	mysqli_query($conn, $sqlQuery) or die("database error:". mysqli_error($conn));	
 	$data = array(
-		"message"	=> "Record Deleted",	
+		"messrating"	=> "Record Deleted",	
 		"status" => 1
 	);
 	echo json_encode($data);	
 }
+
--- Greenlights/js/editable.js
+++ Greenlights/js/editable.js
@@ -1,20 +1,45 @@
 $( document ).ready(function() {
   $('#editableTable').SetEditable({
-	  columnsEd: "0,1,2,3,4,5,6",
+	  columnsEd: "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11",
 	  onEdit: function(columnsEd) {
 		 // console.log("===edit=="+(this));
-		var empId = columnsEd[0].childNodes[1].innerHTML;
-        var empName = columnsEd[0].childNodes[3].innerHTML;
-        var gender = columnsEd[0].childNodes[5].innerHTML;
-        var age = columnsEd[0].childNodes[7].innerHTML;
-        var skills = columnsEd[0].childNodes[9].innerHTML;
-		var address = columnsEd[0].childNodes[11].innerHTML;
+//  group_number 3
+//  rating 4
+//  task_actual 6
+//  comment 7
+//  action 8
+//  meeting_date 9
+//  meeting_duration 10
+        var id = columnsEd[0].childNodes[1].innerHTML;
+        var week = columnsEd[0].childNodes[3].innerHTML;
+        var session = columnsEd[0].childNodes[5].innerHTML;
+        var task = columnsEd[0].childNodes[7].innerHTML;
+		var group_number = columnsEd[0].childNodes[9].innerHTML;
+        var rating = columnsEd[0].childNodes[11].innerHTML;
+        var task_expected = columnsEd[0].childNodes[13].innerHTML;
+        var task_actual = columnsEd[0].childNodes[15].innerHTML;
+        var comment = columnsEd[0].childNodes[17].innerHTML;
+        var action = columnsEd[0].childNodes[19].innerHTML;
+		var meeting_date = columnsEd[0].childNodes[21].innerHTML;
+        var meeting_duration = columnsEd[0].childNodes[23].innerHTML;
 		$.ajax({
 			type: 'POST',			
 			url : "action.php",	
 			dataType: "json",					
-			data: {id:empId, name:empName, gender:gender, age:age, skills:skills, address:address, action:'edit'},			
-			success: function (response) {
+			data: {id:id, 
+                   week:week, 
+                   session:session, 
+                   task:task, 
+                   group_number:group_number, 
+                   rating:rating, 
+                   task_expected:task_expected, 
+                   task_actual:task_actual, 
+                   comment:comment, 
+                   action:action, 
+                   meeting_date:meeting_date, 
+                   meeting_duration:meeting_duration, 
+                   action:'edit'},			
+			 success: function (response) {
 				if(response.status) {
 					// show update message
 				}						
@@ -22,12 +47,12 @@ $( document ).ready(function() {
 		});
 	  },
 	  onBeforeDelete: function(columnsEd) {
-	  var empId = columnsEd[0].childNodes[1].innerHTML;
+	  var id = columnsEd[0].childNodes[1].innerHTML;
 	  $.ajax({
 			type: 'POST',			
 			url : "action.php",
 			dataType: "json",					
-			data: {id:empId, action:'delete'},			
+			data: {id:id, action:'delete'},			
 			success: function (response) {
 				if(response.status) {
 					// show delete message
