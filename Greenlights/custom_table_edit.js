e96aebc710eda5b490f032880d5ae7d0b0f8b35d
Andrey Staniukynas <dev.staniukynas@gmail.com>
2020-08-22 17:34:49 +0300

    Works. All columns are editable

--- Greenlights/custom_table_edit.js
+++ Greenlights/custom_table_edit.js
@@ -4,7 +4,17 @@ $(document).ready(function(){
 		editButton: false,   		
 		columns: {
 		  identifier: [0, 'id'],                    
-		  editable: [[1, 'week'], [2, 'session'], [3, 'task'], [4, 'group_number'], [5, 'rating']]
+		  editable: [[1, 'week'],
+                     [2, 'session'],
+                     [3, 'task'], 
+                     [4, 'group_number'], 
+                     [5, 'rating'],
+                     [6, 'task_expected'], 
+                     [7, 'task_actual'], 
+                     [8, 'comment'], 
+                     [9, 'action'],
+                     [10, 'meeting_date'],
+                     [11, 'meeting_action']]
 		},
 		hideIdentifier: true,
 		url: 'live_edit.php'		
--- Greenlights/live_edit.php
+++ Greenlights/live_edit.php
@@ -10,12 +10,24 @@ if ($input['action'] == 'edit') {
 	} else if(isset($input['task'])) {
 		$update_field.= "task='".$input['task']."'";
 	} else if(isset($input['group_number'])) {
-		$update_field.= "group_number=".$input['group_number']."";
+		$update_field.= "group_number='".$input['group_number']."'";
 	} else if(isset($input['rating'])) {
-		$update_field.= "rating=".$input['rating']."";
-	}	
+		$update_field.= "rating='".$input['rating']."'";
+	} else if(isset($input['task_expected'])) {
+		$update_field.= "task_expected='".$input['task_expected']."'";
+	} else if(isset($input['task_actual'])) {
+		$update_field.= "task_actual='".$input['task_actual']."'";
+	} else if(isset($input['comment'])) {
+		$update_field.= "comment='".$input['comment']."'";
+	} else if(isset($input['action'])) {
+		$update_field.= "action='".$input['action']."'";
+	} else if(isset($input['meeting_date'])) {
+		$update_field.= "meeting_date='".$input['meeting_date']."'";
+	} else if(isset($input['meeting_duration'])) {
+		$update_field.= "meeting_duration='".$input['meeting_duration']."'";
+	}
 	if($update_field && $input['id']) {
-		$sql_query = "UPDATE TA_development.s18182839 SET $update_field WHERE id=" . $input['id'] . "";	
+		$sql_query = "UPDATE TA_development.s18182839 SET $update_field WHERE id='" . $input['id'] . "'";	
 		mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));		
 	}
 }
