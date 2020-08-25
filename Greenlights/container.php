c8aa300ee2064a641cc78d64309b67a7e48238bf
Andrey Staniukynas <dev.staniukynas@gmail.com>
2020-08-26 02:56:12 +0300

    added login to header

--- Greenlights/container.php
+++ Greenlights/container.php
@@ -1,8 +1,6 @@
 </head>
 <body>
 <header class="header">
-    <a href="http://www.ucl.ac.uk" class="navbar-brand">Greenlights</a>
-<!--
     <div role="navigation" class="navbar navbar-default navbar-static-top">
           <div class="container">
                 <div class="navbar-header">
@@ -17,11 +15,20 @@
 
                 <div class="navbar-collapse collapse">
                   <ul class="nav navbar-nav">
-                    <li class="active"><a href="http://www.ucl.ac.uk">Home</a></li>
-                  </ul>
-                </div>/.nav-collapse 
+<!--                    <li class="active"><a href="http://www.ucl.ac.uk">Home</a></li>-->
+                    <?php
+                      $login_url = 'https://6e558e179fd9.ngrok.io/Greenlights/Greenlights/login.php';
+                        if(isset($_SESSION['student_id'])) {
+                            echo '<li class="inactive"><a>Welcome, ' . $_SESSION['given_name'] . '</li>';
+                            echo '<li class="inactive"><a>' . substr($_SESSION['student_id'], 1) . '</li>';
+                            echo '<li class="inactive"><a href="' . $_SESSION['base_url'] . '?logout">Log Out</a></li>';
+                        } else {
+                            echo '<li class="inactive"><a href="'. $login_url .'">Login</a></li>';
+                        }
+                    ?>
+                    </ul>
+                </div>
           </div>
     </div>
--->
 </header>
 <div id="content">
